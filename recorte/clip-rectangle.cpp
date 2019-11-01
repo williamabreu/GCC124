241373ce1c008f92b7cf3908f6739fbbd2fa50d9
williamabreu <williamsantos_00@hotmail.com>
2019-11-01 15:42:11 -0300

    Add recorte sem nada by bruno

---
+++ recorte/clip-rectangle.cpp
@@ -0,0 +1,42 @@
+#include "clip-rectangle.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ClipRectangle::ClipRectangle (double xmin, double xmax, double ymin, double ymax)
+    : mXmin(xmin), mXmax(xmax), mYmin(ymin), mYmax(ymax), mInitialized(true)
+{}
+
+void ClipRectangle::Set(double xmin, double xmax, double ymin, double ymax)
+{
+    mXmin = xmin;
+    mXmax = xmax;
+    mYmin = ymin;
+    mYmax = ymax;
+    mInitialized = true;
+}
+
+// Retorna um ponteiro para uma linha recem alocada inteiramente dentro da area de recorte
+// ou NULL se l for completamente descartada.
+// A linha alocada aqui será desalocada na função CleanUp().
+Line* ClipRectangle::Clip(const Line& l)
+{
+    #error ClipRectangle::Clip não foi implementada.
+}
+
+void ClipRectangle::Read(const string& prompt)
+{
+    cout << prompt;
+    cin >> mXmin >> mXmax >> mYmin >> mYmax;
+    if (not mInitialized)
+        mInitialized = true;
+}
+
+ostream& operator << (ostream& os, const ClipRectangle& r)
+{
+    return os << "(Clipping Rectangle: " << r.mXmin << ", " << r.mXmax << ", " 
+              << r.mYmin << ", " << r.mYmax << ")";
+}
+
+
